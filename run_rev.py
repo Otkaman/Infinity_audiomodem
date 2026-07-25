19d1daa64e0960c73193007f7c5268960297ae5f
Rudichev <petrrudichev@gmail.com>
2026-07-25 21:59:02 +0400

    загрузил

---
+++ run_rev.py
@@ -0,0 +1,4 @@
+from src.windows.modem.receptor import receive_file_from_mic
+
+
+receive_file_from_mic(output_dir="./incoming")
---
+++ src/windows/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
+STYLES_DIR = BASE_DIR / "app" / "styles"
